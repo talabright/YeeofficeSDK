b2c5f3d179bd80a685f29b19fa52ef8cb718d5c7
Terry Tan <terry.tan@akmii.com>
2019-11-06 14:43:41 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # YeeofficeSDK
+Summary：此SDK主要封装了内容列表的接口
+
+调用示例
+
+
+```
+var context = AkmiiContext.GetAkmiiContext("对应环境的域名地址", "知普信息颁发的token");
+var response = context.Repository.SelectByListIDAsync(new CustomDataGetListRequest
+{
+    AppID = 41,
+    ListID = 1153194374738350080
+}).Result;
+```
---
+++ YeeOfficeSDKUnitTest/ListDataUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using YeeOfficeSDK;
+using YeeOfficeSDK.Models;
+
+namespace YeeOfficeSDKUnitTest
+{
+    [TestClass]
+    public class ListDataUnitTest
+    {
+        [TestMethod]
+        public void GetDataByListIDTest()
+        {
+            try
+            {
+                var context = AkmiiContext.GetAkmiiContext(Util.DomainUrl, Util.Secret);
+                var response = context.Repository.SelectByListIDAsync(new CustomDataGetListRequest
+                {
+                    AppID = 41,
+                    ListID = 1153194374738350080
+                }).Result;
+                var count = response.Data;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            Assert.IsTrue(true);
+        }
+    }
+}
